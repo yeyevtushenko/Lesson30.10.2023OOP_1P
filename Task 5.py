24b111401c7b7ba75af93d86a837b6f4ea75e789
yeyevtushenko <yehor803@gmail.com>
2023-11-07 21:21:42 +0200

    створення методу deposit

--- Task 5.py
+++ Task 5.py
@@ -5,3 +5,10 @@ class BankAccount:
     def __init__(self, owner, balance=0):
         self.owner = owner
         self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Поповнення балансу на {amount} грн успішно виконано.")
+        else:
+            print("Помилка: Сума поповнення повинна бути більше нуля.")
