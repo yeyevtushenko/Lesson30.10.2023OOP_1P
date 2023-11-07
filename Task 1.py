a8626205e8faef0ad0314d4e9f759543f78ce550
yeyevtushenko <yehor803@gmail.com>
2023-11-07 19:27:30 +0200

    Створення класу Студент

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+# Створіть клас Student з атрибутами name та age.
+# Додайте метод print_info, який виведе інформацію про студента у на вигляді
+# "Ім'я: {name}, Вік: {age}".
+print("Завдання №1")
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+
