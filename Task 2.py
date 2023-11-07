9795ad4b5573f65fede5619070a3e2548cfc122c
yeyevtushenko <yehor803@gmail.com>
2023-11-07 19:55:19 +0200

    імпортуємо бібліотеку math та створюємо метод коло

---
+++ Task 2.py
@@ -0,0 +1,9 @@
+# Створіть клас Circle з атрибутом radius та методом area,
+# який поверне площу кола з вказаним радіусом.
+print("Завдання №2")
+import math
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
