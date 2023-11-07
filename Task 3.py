b2806c2eb057a3159d482037cf1eb2952db2345c
yeyevtushenko <yehor803@gmail.com>
2023-11-07 20:28:14 +0200

    Додано метод display_info, який виведе інформацію про книгу у вигляді "Назва: {title}, Автор: {author}, Жанр: {genre}".

--- Task 3.py
+++ Task 3.py
@@ -8,3 +8,6 @@ class Book:
         self.title = title
         self.author = author
         self.genre = genre
+
+    def display_info(self):
+        print(f"Назва: {self.title}, Автор: {self.author}, Жанр: {self.genre}")
